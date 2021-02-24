16882fd32021f89df0a27bcf1fc880ab1a82d175
dengyu <2540628013@qq.com>
2021-02-24 18:45:56 +0800

    leetcode

--- src/main/java/com/leetcode/DP/_10.java
+++ src/main/java/com/leetcode/DP/_10.java
@@ -1,5 +1,7 @@
 package com.leetcode.DP;
 
+import jdk.nashorn.internal.ir.IfNode;
+
 public class _10 {
     //    //暴力枚举，双指针
 //    public boolean isMatch(String s, String p) {
@@ -58,9 +60,74 @@ public class _10 {
 //        return true;
 //    }
 //暴力枚举，双指针
+//    public boolean isMatch(String s, String p) {
+//        if ("".equals(s) && "".equals(p)) return true;
+//        if ("".equals(s)) return false;
+//        //如果只有“。*”
+//        if (".*".equals(p)) return true;
+//
+//        char[] strArray = s.toCharArray();
+//        char[] pArray = p.toCharArray();
+//        int j = 0;
+//        int i = 0;
+//        for (; i < pArray.length; ) {
+//            if (j < strArray.length && strArray[j] == pArray[i]) {
+//                j++;
+//                i++;
+//            }
+//            //如果遇到 "."
+//            while (i < pArray.length && '.' == pArray[i]) {
+//                j++;
+//                i++;
+//            }
+//            //如果遇到“*”
+////            if (j == strArray.length && ((i < pArray.length && pArray[i] == '*')||(i+1<pArray.length&&pArray[i+1]=='*'))) {
+//            if (j == strArray.length && ((i < pArray.length && pArray[i] == '*'))) {
+//                //去掉i-1和i
+//                int k = 1;
+//                while (j - k >= 0) {
+//                    if (isMatch(s.substring(j - k, strArray.length), p.substring(i + k, p.length()))) return true;
+//                    if (i-1>=0&&isMatch(s.substring(j - k, strArray.length), p.substring(i -1, p.length()))) i++;
+//                    k++;
+//                }
+//            }
+//            //如果最后一个是*
+//            if (i < pArray.length && j < strArray.length && strArray[j] != pArray[i]) {
+//                if (pArray[i] == '*') {
+//                    if (i - 1 < 0) return false;
+//                    char preStr = pArray[i - 1];
+//                    //*代替一个以上
+//                    while (j < strArray.length && strArray[j] == preStr) {
+//                        if (isMatch(s.substring(j, s.length()), p.substring(i + 1, p.length()))) return true;
+//                        j++;
+//                    }
+//                    i++;
+//                } else if (i + 1 < pArray.length && pArray[++i] == '*') {
+//                    char preStr = pArray[i];
+//                    //*代替一个以上
+//                    while (j < strArray.length && strArray[j] == preStr) {
+//                        if (isMatch(s.substring(j, s.length()), p.substring(i, p.length()))) return true;
+//                        j++;
+//                    }
+//                    i++;
+//                } else {
+//                    return false;
+//                }
+//
+//            }
+//            if (j == strArray.length && i < pArray.length) return false;
+//            // if (j < strArray.length&&i < pArray.length&&pArray[i] != strArray[j]) return false;
+//            ////如果j扫描完了
+//
+//        }
+//
+//        if (j < strArray.length) return false;
+//
+//        return true;
+//    }
+
     public boolean isMatch(String s, String p) {
         if ("".equals(s) && "".equals(p)) return true;
-        if ("".equals(s)) return false;
         //如果只有“。*”
         if (".*".equals(p)) return true;
 
@@ -69,9 +136,7 @@ public class _10 {
         int j = 0;
         int i = 0;
         for (; i < pArray.length; ) {
-            ////如果j扫描完了
-            if (j == strArray.length && i < pArray.length) return false;
-            if (j < strArray.length && strArray[j] == pArray[i]) {
+            while (j < strArray.length && i < pArray.length && strArray[j] == pArray[i]) {
                 j++;
                 i++;
             }
@@ -80,30 +145,37 @@ public class _10 {
                 j++;
                 i++;
             }
-            //如果遇到“*”
-            if (i < pArray.length && '*' == pArray[i]) {
-                char preStr = pArray[i - 1];
-                //代替0个
-                if (j == strArray.length ){
-                    if (isMatch(s.substring(j-1, s.length()), p.substring(i + 1, p.length()))) return true;
-                }
 
-                //*代替一个以上
-                while (j < strArray.length && strArray[j] == preStr) {
+            //如果遇到*
+            int k = i;
+            if (k < pArray.length && pArray[k] != '*') {
+                k++;
+            }
+            //k就是遇到的*
+            //第一种情况代表0个
+            if (k < pArray.length && pArray[k] == '*') {
+                if (j-1>=0&&isMatch(s.substring(j-1, s.length()), p.substring(k + 1, p.length()))) return true;
+                //第二种情况代替1个以上 k-1；
+                while (j < strArray.length && (pArray[k - 1] == strArray[j] || pArray[k - 1] == '.')) {
+                    if (isMatch(s.substring(j, s.length()), p.substring(k + 1, p.length()))) return true;
                     j++;
-                    if (isMatch(s.substring(j, s.length()), p.substring(i + 1, p.length()))) return true;
                 }
+                i = k + 1;
+            }
+            //如果到这里已经扫描完了
+            if (j == strArray.length - 1 && i == pArray.length - 1 && strArray[j] == pArray[i]) return true;
+            if (j == strArray.length && i < pArray.length && pArray[i] != '*') return false;
 
-
+            if (j < strArray.length && i < pArray.length && pArray[i] != strArray[j] && pArray[i] != '.') return false;
+            if (i != k + 1) {
                 i++;
             }
-            if (j < strArray.length&&i < pArray.length&&pArray[i] != strArray[j]) return false;
         }
 
         if (j < strArray.length) return false;
+
         return true;
     }
-
 
     public static void main(String[] args) {
         //"abbba"
@@ -118,10 +190,21 @@ public class _10 {
 
         //"aaa"
         //"ab*ac*a"
-        String s = "aab";
-        String p = "c*a*b";
+//        String s = "aaa";
+//        String p = "aaa*av";
 //        String s = "mississippi";
-//        String p = "mis*is*ip*.";
+//        String p = "mis*is*p*.";
+        //"aba"
+        //"a.*aa"
+        //"ab"
+        //".*c"
+//        String s = "ab";
+//        String p = ".*c";
+        //"a"
+        //"ab*a"
+        String s = "a";
+        String p = "ab*a";
         System.out.println(new _10().isMatch(s, p));
+//        System.out.println(s.substring(1, 1));
     }
 }
